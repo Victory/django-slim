dafdd811531dd8fbd2c8f503aa45dbb71ebbce82
vic <git@dfhu.org>
2014-05-25 14:32:14 -0400

    add version check to test

--- run-tests.py
+++ run-tests.py
@@ -1,2 +1,19 @@
-print "No Tests Yet"
-exit(0)
+import django
+import sys
+
+
+def pretty_assertish(msg, cond):
+    sys.stdout.write('%s...' % msg)
+    if cond:
+        print "Passed"
+        return 0
+    else:
+        print "Failed"
+        return 1
+
+errs = 0
+errs += pretty_assertish(
+    "Checking Version Number",
+    django.get_version() == '1.7b4');
+
+exit(errs)
