206e5ed957e4ac8c6dfe68ef2a8de6fb37be7006
vic <git@dfhu.org>
2014-05-27 18:24:55 -0400

    first functional test

--- run-tests.py
+++ run-tests.py
@@ -1,6 +1,14 @@
 import django
 import sys
 
+from subprocess import call
+from pyvirtualdisplay import Display
+
+
+print "Starting Virtual Display"
+display = Display(visible=0, size=(1440, 900))
+display.start()
+
 
 def pretty_assertish(msg, cond):
     sys.stdout.write('%s...' % msg)
@@ -15,5 +23,9 @@ errs = 0
 errs += pretty_assertish(
     "Checking Version Number",
     django.get_version() == '1.7b4');
+
+
+print "Running `home` module tests"
+errs = call(['python', 'slim/manage.py', 'test', 'home'])
 
 exit(errs)
--- slim/home/tests.py
+++ slim/home/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+from django.test import LiveServerTestCase
+
+
+class LiveValidationTestCase(LiveServerTestCase):
+    """
+    run tests on ajaxy live validation
+    """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_homepage_200(self):
+        self.browser.get(self.live_server_url + "/magic-bunny")
