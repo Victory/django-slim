d4aac26903ec49c5714e1a5e3af7daa1e1b256ae
vic <git@dfhu.org>
2014-06-08 17:18:25 -0400

    add static file support

--- slim/slim/settings.py
+++ slim/slim/settings.py
@@ -80,4 +80,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+print STATICFILES_DIRS
 STATIC_URL = '/static/'
