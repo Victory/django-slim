28f4ee5d14edbff94871ddfd318127a6e4578608
vic <git@dfhu.org>
2014-07-05 05:54:56 -0400

    make info tip a real, as of yet unprocessed, form

--- slim/infoorg/views.py
+++ slim/infoorg/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from infoorg.forms import InfoTipForm
 
 from django.views.generic import (
     ListView,
@@ -13,3 +15,9 @@ from django.views.generic import (
 
 class HomeView(TemplateView):
     template_name = "info-home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['action'] = reverse('info-home')
+        context['form'] = InfoTipForm()
+        return context
