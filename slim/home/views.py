51c37d80fb5c698040e2019b33fb58efe77c67da
vic <git@dfhu.org>
2014-07-08 18:51:09 -0400

    example of returning json using a view mixin

--- slim/home/views.py
+++ slim/home/views.py
@@ -1,5 +1,7 @@
-from django.utils.translation import ugettext as _
+import json
 
+from django.utils.translation import ugettext as _
+from django.http import HttpResponse
 
 from django.views.generic import (
     ListView,
@@ -10,5 +12,22 @@ from django.views.generic import (
     TemplateView)
 
 
+class JSONResponseMixin(object):
+    def json_response(self, context, **kwargs):
+        return HttpResponse(
+            json.dumps(context),
+            content_type='text/json',
+            **kwargs)
+
 class DetailSlimView(TemplateView):
     template_name = 'home.html'
+
+
+class SomeJsonView(JSONResponseMixin, TemplateView):
+    def get_context_data(self, **kwargs):
+        context = {}
+        context['some_data'] = "some data"
+        return context
+
+    def render_to_response(self, context, **kwargs):
+        return self.json_response(context, **kwargs)
--- slim/slim/urls.py
+++ slim/slim/urls.py
@@ -12,6 +12,7 @@ js_info_dict = {
 
 urlpatterns = patterns('',
     url(r'^$', home.views.DetailSlimView.as_view(), name='home'),
+    url(r'^json/$', home.views.SomeJsonView.as_view(), name='json'),
     url(r'^infoorg/$', infoorg.views.HomeView.as_view(), name='info-home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
