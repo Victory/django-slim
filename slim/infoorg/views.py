da8e080b5229b9ff2f194cf51b2b42cbe1837a20
vic <git@dfhu.org>
2014-07-10 18:46:02 -0400

    make info tip process form

--- slim/infoorg/views.py
+++ slim/infoorg/views.py
@@ -27,16 +27,26 @@ class HomeView(FormView):
 
 class InfoTipView(JSONResponseMixin, FormView):
     form_class = InfoTipForm
+    success_url = '/infoorg/infotip'
+
+    def form_invalid(self, form):
+        context = super(InfoTipView, self).form_valid(form)
+        print context
+        context['form_submited'] = True
+        context['form_valid'] = False
+        return context
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
+        print "WOOOW"
         form.send_mail()
         return super(InfoTipView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = {}
         context['form_submited'] = True
+        context['form_valid'] = True
         return context
 
     def render_to_response(self, context, **kwargs):
